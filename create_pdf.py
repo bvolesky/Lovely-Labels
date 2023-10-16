16b8cf88391aa7ea4ee5609094bd182b0c468931
Brandon Volesky <brandon.volesky@hrblock.com>
2023-10-16 18:33:11 -0500

    commit

--- create_pdf.py
+++ create_pdf.py
@@ -1,81 +1,183 @@
-import os
-from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
-from PIL import Image, ImageOps
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
 
-class Label:
-    def __init__(self, first_name, last_name, address, city, state, zip_code, image_path):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zip_code = zip_code
-        self.image = self.process_image(image_path)
-
-    def process_image(self, image_path):
-        image = Image.open(image_path)
-        new_width = IMAGE_SIZE * INCH_TO_POINTS
-        aspect_ratio = image.width / image.height
-        new_height = new_width / aspect_ratio
-        image = ImageOps.fit(image, (int(new_width), int(new_height)))
-        return image
 
 class Sheet:
-    def __init__(self, file_name, pagesize=letter):
-        self.file_name = file_name
-        self.pagesize = pagesize
-        self.c = canvas.Canvas(self.file_name, pagesize=self.pagesize)
-        self.c.setFont(FONT_NAME, FONT_SIZE)
+    def __init__(self, width=8.5, height=11, x_margin=0.19, y_margin=0.5, line_width=0.3):
+        self.width = width
+        self.height = height
+        self.x_margin = x_margin
+        self.y_margin = y_margin
+        self.canvas = canvas.Canvas("address_labels.pdf", pagesize=letter)
+        self.canvas.setLineWidth(line_width)
+class LabelGroup:
+    # Create a label group that is the size of the sheet inside margins
+    def __init__(self, sheet):
+        self.sheet = sheet
+        self.x = sheet.x_margin
+        self.y = sheet.y_margin
+        self.width = sheet.width - (sheet.x_margin * 2)
+        self.height = sheet.height - (sheet.y_margin * 2)
+        self.canvas = sheet.canvas
 
-    def draw_label(self, x, y, label):
-        self.c.drawInlineImage(
-            label.image,
-            x,
-            y + (LABEL_HEIGHT - label.image.height) / IMAGE_Y_DIVISION,
-            width=label.image.width,
-            height=label.image.height,
-        )
-        text_x = x + label.image.width + IMAGE_TEXT_SPACING_X
-        self.c.drawString(text_x, y + LABEL_HEIGHT - LINE_1_POSITION_Y, f"{label.first_name} {label.last_name}")
-        self.c.drawString(text_x, y + LABEL_HEIGHT - LINE_2_POSITION_Y, label.address)
-        self.c.drawString(text_x, y + LABEL_HEIGHT - LINE_3_POSITION_Y, f"{label.city}, {label.state} {label.zip_code}")
+    # Draw the margins of the label group
+    def draw_margins(self, fill=False):
+        self.canvas.rect(self.x * inch, self.y * inch, self.width * inch,
+                         self.height * inch)
+        if fill:
+            self.canvas.setFillColorRGB(0, 0, 0)
+            self.canvas.rect(self.x * inch, self.y * inch, self.width * inch,
+                             self.height * inch, fill=1)
 
-    def generate_labels(self, label):
-        for column in range(NUM_ACROSS):
-            for row in range(NUM_DOWN):
-                x = SIDE_MARGIN + column * (LABEL_WIDTH + GAP_BETWEEN_LABELS_X)
-                y = TOP_MARGIN + (NUM_DOWN - ALL_LABEL_GROUP_POSITION_Y - row) * LABEL_HEIGHT
-                self.draw_label(x, y, label)
+# Create a class called Label that is the size of a label in the matrix
+class Label:
+    # Size of label is for AVERY 5160
+    # Labels should have 0.118 spacing between them horizontally on the right
+    # and NO spacing vertically
 
-    def save(self):
-        self.c.save()
-        os.system(f"start {self.file_name}")
+    def __init__(self, label_group, x, y,data, width=2.625, height=1, bypass=False):
+        self.label_group = label_group
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.canvas = label_group.canvas
+        self.address_group = AddressGroup(self, data)
+        if not bypass:
+            self.address_group.draw()
 
-# Constants
-FILE_NAME = "labels.pdf"
-INCH_TO_POINTS = 72
-LABEL_WIDTH = 2.63 * INCH_TO_POINTS
-LABEL_HEIGHT = 1 * INCH_TO_POINTS
-TOP_MARGIN = 0.5 * INCH_TO_POINTS
-SIDE_MARGIN = 0.19 * INCH_TO_POINTS
-NUM_ACROSS = 3
-NUM_DOWN = 10
-IMAGE_SIZE = 0.85
-FONT_NAME = "Helvetica"
-FONT_SIZE = 10
-IMAGE_Y_DIVISION = 2
-IMAGE_TEXT_SPACING_X = 10
-GAP_BETWEEN_LABELS_X = 10
-ALL_LABEL_GROUP_POSITION_Y = 1
-SPACING = 15
-LINE_1_POSITION_Y = 25
-LINE_2_POSITION_Y = LINE_1_POSITION_Y + SPACING
-LINE_3_POSITION_Y = LINE_2_POSITION_Y + SPACING
+    def draw(self, fill=False):
+        self.canvas.rect(self.x * inch, self.y * inch, self.width * inch,
+                         self.height * inch)
+        if fill:
+            self.canvas.setFillColorRGB(0, 0, 0)
+            self.canvas.rect(self.x * inch, self.y * inch, self.width * inch,
+                             self.height * inch, fill=1)
 
-label = Label(
-    "Johnny", "Appleseed", "123 Main Street", "Springfield", "KS", "66203", "cat.jpg"
-)
-sheet = Sheet(FILE_NAME)
-sheet.generate_labels(label)
-sheet.save()
+
+# Create an address group that contains all text lines inside the label
+class AddressGroup:
+    def __init__(self, label,data, padding=0.15):
+        self.label = label
+        self.padding = padding  # Introduce a padding for AddressGroup
+        self.width = label.width * 0.5
+        self.height = label.height - 2 * self.padding
+        self.x = label.x + self.width - self.padding
+        self.y = label.y + self.padding
+        self.canvas = label.canvas
+        self.line_height = 0.2
+        self.line_spacing = 0.1
+        self.data = data
+
+
+    def draw(self, fill=False):
+        self.canvas.rect(self.x * inch, self.y * inch, self.width * inch, self.height * inch)
+        full_name = self.data['first_name'] + ' ' + self.data['last_name']
+        address_line_1 = full_name
+        address_line_2 = self.data['address']
+        address_line_3 = self.data['city'] + ', ' + self.data['state'] + ' ' + self.data['zip']
+        address_lines = [address_line_1, address_line_2, address_line_3]
+        reversed_address_lines = address_lines[::-1]
+        for index,item in enumerate(reversed_address_lines):
+            address_line = AddressLine(self, index, item)
+            address_line.draw(fill)
+
+        if fill:
+            self.canvas.setFillColorRGB(0, 0, 0)
+            self.canvas.rect(self.x * inch, self.y * inch, self.width * inch, self.height * inch, fill=1)
+
+
+class Text:
+    def __init__(self, content):
+        self.content = content
+        self.font = "Helvetica"
+        self.size = 12
+
+
+    def draw(self, canvas, x, y):
+        canvas.drawString(x, y, self.content)
+
+
+class AddressLine:
+    def __init__(self, label, line_number, text, num_lines=3):
+        self.label = label
+        self.line_number = line_number
+        self.num_lines = num_lines
+        self.x = label.x
+        self.y = label.y + (self.line_number * (label.height / self.num_lines))
+        self.width = label.width
+        self.height = label.height / self.num_lines
+        self.canvas = label.canvas
+        self.text = Text(text)
+
+    def draw(self, fill=False):
+        self.canvas.rect(self.x * inch, self.y * inch, self.width * inch,
+                         self.height * inch)
+
+        # Assuming the Text class has a method getHeight() to get the height of the text in inches
+
+        # Adjust the y position to center the text vertically
+        self.text.draw(self.canvas, self.x * inch,
+                       (self.y + 0.05) * inch)
+
+        if fill:
+            self.canvas.setFillColorRGB(0, 0, 0)
+            self.canvas.rect(self.x * inch, self.y * inch, self.width * inch,
+                             self.height * inch, fill=1)
+
+
+
+
+# Create a label matrix in the label group that is 3x10
+class LabelMatrix:
+    def __init__(self, label_group, label, data):
+        self.num_rows = 10
+        self.num_cols = 3
+        self.horizontal_spacing = 0.118  # Introducing horizontal spacing between labels
+        self.label_group = label_group
+        self.label = label
+        self.x = label_group.x
+        self.y = label_group.y
+        self.width = label_group.width
+        self.height = label_group.height
+        self.canvas = label_group.canvas
+        self.matrix = []
+        self.data = data
+        self.create_matrix()
+
+
+    def create_matrix(self):
+        self.matrix = [
+            [
+                Label(
+                    self.label_group,
+                    self.x + i * (self.label.width + self.horizontal_spacing),
+                    self.y + j * self.label.height,
+                    self.data
+                )
+                for i in range(self.num_cols)
+            ]
+            for j in range(self.num_rows)
+        ]
+
+    def draw(self, location=False, fill=False):
+        if not location:
+            for row in self.matrix:
+                for label in row:
+                    label.draw(fill)
+        else:
+            # location is a tuple of (row, col)
+            self.matrix[location[0]][location[1]].draw(fill)
+
+
+
+# Usage
+my_sheet = Sheet()
+my_label_group = LabelGroup(my_sheet)
+my_label_group.draw_margins()
+data = {'first_name': 'John', 'last_name': 'Doe', 'address': '123 Main St.', 'city': 'Anytown', 'state': 'KS', 'zip': '12345'}
+my_label = Label(my_label_group, 0, 0,data, bypass=True)
+my_label_matrix = LabelMatrix(my_label_group, my_label,data)
+my_label_matrix.draw()
+my_sheet.canvas.save()
