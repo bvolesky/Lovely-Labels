1a30bd81ee2aaf1400d7d4f128eda29237709966
Brandon Volesky <brandon.volesky@hrblock.com>
2023-10-17 09:51:29 -0500

    commit

--- create_pdf.py
+++ create_pdf.py
@@ -2,12 +2,27 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 
-# Constants
+# Global Constants
 DEFAULT_WIDTH = 8.5
 DEFAULT_HEIGHT = 11
 DEFAULT_X_MARGIN = 0.19
 DEFAULT_Y_MARGIN = 0.5
 DEFAULT_LINE_WIDTH = 0.3
+
+NUM_ROWS = 10
+NUM_COLS = 3
+HORIZONTAL_SPACING = 0.118
+LABEL_WIDTH = 2.625
+LABEL_HEIGHT = 1
+ADDRESS_GROUP_PADDING = 0.15
+IMAGE_GROUP_PADDING = 0.15
+ADDRESS_LINE_HEIGHT = 0.23
+ADDRESS_LINE_SPACING = 0.0
+
+FONT_NAME = "Helvetica"
+FONT_SIZE = 10
+TEXT_X_OFFSET = 0.025
+TEXT_Y_OFFSET = 0.05
 
 class Sheet:
     def __init__(
@@ -29,14 +44,14 @@ class Sheet:
         self._draw_margins()
 
     def _draw_margins(self, fill=False):
-        self._rect_with_optional_fill(self.x_margin, self.y_margin, self.width - 2 * self.x_margin, self.height - 2 * self.y_margin, fill)
+        if self.margin_outline:
+            self._rect_with_optional_fill(self.x_margin, self.y_margin, self.width - 2 * self.x_margin, self.height - 2 * self.y_margin, fill)
 
     def _rect_with_optional_fill(self, x, y, width, height, fill):
         self.canvas.rect(x * inch, y * inch, width * inch, height * inch)
         if fill:
             self.canvas.setFillColorRGB(0, 0, 0)
             self.canvas.rect(x * inch, y * inch, width * inch, height * inch, fill=1)
-
 
 class LabelMatrix:
     def __init__(
@@ -48,9 +63,9 @@ class LabelMatrix:
         address_lines_outline=False,
         image_outline=False,
     ):
-        self.num_rows = 10
-        self.num_cols = 3
-        self.horizontal_spacing = 0.118
+        self.num_rows = NUM_ROWS
+        self.num_cols = NUM_COLS
+        self.horizontal_spacing = HORIZONTAL_SPACING
         self.sheet = sheet
         self.sample_label = self._create_sample_label(data)
         self.x = sheet.x_margin
@@ -99,13 +114,7 @@ class LabelMatrix:
             # location is a tuple of (row, col)
             self.matrix[location[0]][location[1]].draw(fill)
 
-
-# Create a class called Label that is the size of a label in the matrix
 class Label:
-    # Size of label is for AVERY 5160
-    # Labels should have 0.118 spacing between them horizontally on the right
-    # and NO spacing vertically
-
     def __init__(
         self,
         label_group,
@@ -116,8 +125,8 @@ class Label:
         address_outline=False,
         address_lines_outline=False,
         image_outline=False,
-        width=2.625,
-        height=1,
+        width=LABEL_WIDTH,
+        height=LABEL_HEIGHT,
         bypass=False,
     ):
         self.label_group = label_group
@@ -159,107 +168,74 @@ class Label:
                 fill=1,
             )
 
-
-# Create an address group that contains all text lines inside the label
 class AddressGroup:
     def __init__(
-        self, label, data, address_outline, address_lines_outline, padding=0.15
+        self, label, data, address_outline, address_lines_outline, padding=ADDRESS_GROUP_PADDING
     ):
         self.label = label
-        self.padding = padding  # Introduce a padding for AddressGroup
+        self.padding = padding
         self.width = label.width * 0.5
         self.height = label.height - 2 * self.padding
         self.x = label.x + self.width - self.padding
         self.y = label.y + self.padding
         self.canvas = label.canvas
-        self.line_height = 0.2
-        self.line_spacing = 0.1
+        self.line_height = ADDRESS_LINE_HEIGHT
+        self.line_spacing = ADDRESS_LINE_SPACING
+        self.address_lines = []
         self.data = data
         self.address_outline = address_outline
         self.address_lines_outline = address_lines_outline
+        self._create_address_lines()
 
-    def draw(self, fill=False):
+    def _create_address_lines(self):
+        line_data = [
+            self.data["first_name"] + " " + self.data["last_name"],
+            self.data["address"],
+            self.data["city"] + ", " + self.data["state"] + " " + self.data["zip"],
+        ]
+        for i, data in enumerate(line_data):
+            y = self.y + i * (self.line_height + self.line_spacing)
+            line = AddressLine(self, self.x, y, data, self.address_lines_outline)
+            self.address_lines.append(line)
+
+    def draw(self):
         if self.address_outline:
             self.canvas.rect(
                 self.x * inch, self.y * inch, self.width * inch, self.height * inch
             )
-        full_name = self.data["first_name"] + " " + self.data["last_name"]
-        address_line_1 = full_name
-        address_line_2 = self.data["address"]
-        address_line_3 = (
-            self.data["city"] + ", " + self.data["state"] + " " + self.data["zip"]
-        )
-        address_lines = [address_line_1, address_line_2, address_line_3]
-        reversed_address_lines = address_lines[::-1]
-        for index, item in enumerate(reversed_address_lines):
-            address_line = AddressLine(
-                self, index, item, address_lines_outline=self.address_lines_outline
-            )
-            address_line.draw(fill)
-
-        if fill:
-            self.canvas.setFillColorRGB(0, 0, 0)
-            self.canvas.rect(
-                self.x * inch,
-                self.y * inch,
-                self.width * inch,
-                self.height * inch,
-                fill=1,
-            )
-
+        for line in self.address_lines:
+            line.draw()
 
 class AddressLine:
     def __init__(
-        self, label, line_number, text, num_lines=3, address_lines_outline=False
+        self, address_group, x, y, text, address_lines_outline, height=ADDRESS_LINE_HEIGHT
     ):
-        self.label = label
-        self.line_number = line_number
-        self.num_lines = num_lines
-        self.x = label.x
-        self.y = label.y + (self.line_number * (label.height / self.num_lines))
-        self.width = label.width
-        self.height = label.height / self.num_lines
-        self.canvas = label.canvas
-        self.text = Text(text)
+        self.address_group = address_group
+        self.x = x
+        self.y = y
+        self.text = text
+        self.height = height
+        self.width = address_group.width
+        self.canvas = address_group.canvas
         self.address_lines_outline = address_lines_outline
 
-    def draw(self, fill=False):
-        # Adjust the y position to center the text vertically
-        self.text.draw(self.canvas, (self.x + 0.025) * inch, (self.y + 0.05) * inch)
-
+    def draw(self):
+        self.canvas.setFont(FONT_NAME, FONT_SIZE)
+        self.canvas.drawString(
+            (self.x + TEXT_X_OFFSET) * inch,
+            (self.y + TEXT_Y_OFFSET) * inch,
+            self.text,
+        )
         if self.address_lines_outline:
             self.canvas.rect(
                 self.x * inch, self.y * inch, self.width * inch, self.height * inch
             )
 
-        if fill:
-            self.canvas.setFillColorRGB(0, 0, 0)
-            self.canvas.rect(
-                self.x * inch,
-                self.y * inch,
-                self.width * inch,
-                self.height * inch,
-                fill=1,
-            )
-
-
-class Text:
-    def __init__(self, content):
-        self.content = content
-        self.font = "Helvetica"
-        self.size = 10
-
-    def draw(self, canvas, x, y):
-        canvas.setFont(self.font, self.size)  # Set the font and size before drawing
-        canvas.drawString(x, y, self.content)
-
-
-# Create an image group class that is the container for the image, the image group is similar to the address group
 class ImageGroup:
-    def __init__(self, label, image, padding=0.15, image_outline=False):
+    def __init__(self, label, image, image_outline=False, padding=IMAGE_GROUP_PADDING):
         self.label = label
         self.padding = padding
-        self.width = label.width * 0.35
+        self.width = label.width * 0.5 - 2 * self.padding
         self.height = label.height - 2 * self.padding
         self.x = label.x + self.padding
         self.y = label.y + self.padding
@@ -267,36 +243,21 @@ class ImageGroup:
         self.image = image
         self.image_outline = image_outline
 
-    def draw(self, fill=False):
-        self.image.draw(
-            self.canvas,
-            self.x * inch,
-            self.y * inch,
-            self.width * inch,
-            self.height * inch,
-        )
+    def draw(self):
+        self.image.draw(self.canvas, self.x, self.y, self.width, self.height)
         if self.image_outline:
             self.canvas.rect(
                 self.x * inch, self.y * inch, self.width * inch, self.height * inch
             )
-        if fill:
-            self.canvas.setFillColorRGB(0, 0, 0)
-            self.canvas.rect(
-                self.x * inch,
-                self.y * inch,
-                self.width * inch,
-                self.height * inch,
-                fill=1,
-            )
-
 
 class Image:
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, image_path):
+        self.image_path = image_path
 
-    def draw(self, canvas, x, y, width, height):
-        canvas.drawImage(self.path, x, y, width, height)
-
+    def draw(self,canvas, x, y, width, height):
+        canvas.drawImage(
+            self.image_path, x * inch, y * inch, width * inch, height * inch
+        )
 
 # Usage
 data = {
@@ -309,11 +270,11 @@ data = {
     "image_path": "A.jpg",
 }
 
-MARGIN_OUTLINE = True
-LABEL_OUTLINE = True
-ADDRESS_OUTLINE = True
-ADDRESS_LINES_OUTLINE = True
-IMAGE_OUTLINE = True
+MARGIN_OUTLINE = False
+LABEL_OUTLINE = False
+ADDRESS_OUTLINE = False
+ADDRESS_LINES_OUTLINE = False
+IMAGE_OUTLINE = False
 
 my_sheet = Sheet(margin_outline=MARGIN_OUTLINE)
 my_label_matrix = LabelMatrix(
