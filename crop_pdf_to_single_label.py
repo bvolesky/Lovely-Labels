c5020092ea4e5573a59400fe05cad6842bb2d30e
Brandon Volesky <93208198+bvolesky@users.noreply.github.com>
2024-01-16 13:52:13 -0600

    Added script to add preview label to gui

--- crop_pdf_to_single_label.py
+++ crop_pdf_to_single_label.py
@@ -1,34 +1,18 @@
-import fitz  # PyMuPDF
+import fitz
 from PIL import Image
 
-# PDF file path
-pdf_file_path = "output/address_labels.pdf"
 
-# Output JPG file path
-output_file_path = "output/single_address_label.jpg"
-
-# Specify the coordinates for cropping (replace with your values)
-x1, y1, x2, y2 = 20, 330, 225, 395  # left, upper, right, lower
-
-# Open the PDF file
-pdf_document = fitz.open(pdf_file_path)
-
-# Select the first page (you can change this if necessary)
-page = pdf_document[0]
-
-# Render the page as an image
-pix = page.get_pixmap()
-
-# Create a new image from the PDF content
-image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-
-# Crop the image to the specified coordinates
-cropped_image = image.crop((x1, y1, x2, y2))
-
-# Save the cropped image as a JPG file
-cropped_image.save(output_file_path, "JPEG")
-
-# Close the PDF document
-pdf_document.close()
-
-print(f"Single address label saved as {output_file_path}")
+def create_single_label():
+    pdf_file_path = "output/address_labels.pdf"
+    output_file_path = "output/single_address_label.png"
+    x1, y1, x2, y2 = 20, 330, 225, 395  # left, upper, right, lower
+    pdf_document = fitz.open(pdf_file_path)
+    page = pdf_document[0]
+    pix = page.get_pixmap()
+    image = Image.frombytes("RGB", (pix.width, pix.height), pix.samples)
+    cropped_image = image.crop((x1, y1, x2, y2))
+    new_width = int(cropped_image.width * 1.3)
+    new_height = int(cropped_image.height * 1.3)
+    resized_image = cropped_image.resize((new_width, new_height), Image.BILINEAR)
+    resized_image.save(output_file_path, "PNG")
+    pdf_document.close()
